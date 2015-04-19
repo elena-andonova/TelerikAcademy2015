95a824db7dbc294ea6988947e295263a125bfca8
elena-andonova <ellie.andonova@gmail.com>
2015-04-19 17:16:24 +0300

    update

---
+++ OOP/06.CommonTypeSystem/01-03.StudentClass-IClonable-IComparable/Student.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_03.StudentClass_IClonable_IComparable
+{
+    /*Problem 1. Student class
+    Define a class Student, which contains data about a student – first, middle and last name, SSN, permanent address, mobile phone e-mail, course, specialty, university, faculty. Use an enumeration for the specialties, universities and faculties.
+    Override the standard methods, inherited by System.Object: Equals(), ToString(), GetHashCode() and operators == and !=.
+     
+     * Problem 2. IClonable
+    Add implementations of the ICloneable interface. The Clone() method should deeply copy all object's fields into a new object of type Student.
+
+    * Problem 3. IComparable
+    Implement the IComparable<Student> interface to compare students by names (as first criteria, in lexicographic order) and by social security number (as second criteria, in increasing order).
+     */
+
+    class Student : ICloneable, IComparable<Student>
+    {
+        public enum Specialties { SSS, Arh, ViK, TS, Hidro, Goedesy, El, CompSciences, SoftEng, Biology }
+        public enum Univercities { UACEG, SU, TU, TS }
+        public enum Faculties { SF, AF, FMI, TF, HF, GF }
+
+        private string firstName;
+        private string middleName;
+        private string lastName;
+        private string ssn;
+        private string permanentAddress;
+        private string phone;
+        private string email;
+        private int course;
+        private Specialties speciality;
+        private Univercities university;
+        private Faculties faculty;
+
+        public Student(string fname, string mname, string lname, string ssn, string permAddr,
+            string phone, int course, Specialties speciality, Univercities university, Faculties faculty)
+        {
+            this.FirstName = fname;
+            this.MiddleName = mname;
+            this.LastName = lname;
+            this.SSN = ssn;
+            this.PermanentAddress = permAddr;
+            this.Phone = phone;
+            this.Course = course;
+            this.Speciality = speciality;
+            this.University = university;
+            this.Faculty = faculty;
+        }
+        public string FirstName
+        {
+            get { return this.firstName; }
+            set
+            {
+                this.firstName = value;
+            }
+        }
+        public string MiddleName
+        {
+            get { return this.middleName; }
+            set
+            {
+                this.middleName = value;
+            }
+        }
+        public string LastName
+        {
+            get { return this.lastName; }
+            set
+            {
+                this.lastName = value;
+            }
+        }
+        public string SSN
+        {
+            get { return this.ssn; }
+            set
+            {
+                this.ssn = value;
+            }
+        }
+        public string PermanentAddress
+        {
+            get { return this.permanentAddress; }
+            set
+            {
+                this.permanentAddress = value;
+            }
+        }
+        public string Phone
+        {
+            get { return this.phone; }
+            set
+            {
+                this.phone = value;
+            }
+        }
+        public string Email
+        {
+            get { return this.email; }
+            set
+            {
+                if (!value.Contains('@'))
+                {
+                    throw new ArgumentException("enter valid email");
+                }
+                this.email = value;
+            }
+        }
+        public int Course
+        {
+            get { return this.course; }
+            set
+            {
+                if (value < 0 || value > 8)
+                {
+                    throw new ArgumentException("courses are between 1 and 8");
+                }
+                this.course = value;
+            }
+        }
+        public Specialties Speciality
+        {
+            get { return this.speciality; }
+            set { this.speciality = value; }
+        }
+        public Univercities University
+        {
+            get { return this.university; }
+            set { this.university = value; }
+        }
+        public Faculties Faculty
+        {
+            get { return this.faculty; }
+            set { this.faculty = value; }
+        }
+
+        public override string ToString()
+        {
+            string forPrint = string.Format("{0} {1} SSN: {2}", this.FirstName, this.LastName, this.SSN);
+            return (forPrint);
+        }
+        public override int GetHashCode()
+        {
+            return (int.Parse(this.SSN) * 7 + this.SSN.Length * 13);
+        }
+        public static bool operator !=(Student st1, Student st2)
+        {
+            return !(st1.Equals(st2));
+        }
+        public static bool operator ==(Student st1, Student st2)
+        {
+            return st1.Equals(st2);
+        }
+        public override bool Equals(object obj)
+        {
+            Student st2 = obj as Student;
+            if (Object.Equals(st2, null))
+            {
+                return false;
+            }
+            if (Object.Equals(this.SSN, st2.SSN))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public object Clone()
+        {
+            return new Student(this.FirstName, this.MiddleName, this.LastName, this.SSN, this.PermanentAddress,
+                this.Phone, this.Course, this.Speciality, this.University, this.Faculty);
+        }
+
+        public int CompareTo(Student st2)
+        {
+
+            int fNameComp = this.FirstName.CompareTo(st2.FirstName);
+            if (fNameComp != 0)
+            {
+                return fNameComp;
+            }
+            int mNameComp = this.MiddleName.CompareTo(st2.MiddleName);
+            if (mNameComp != 0)
+            {
+                return mNameComp;
+            }
+            int lNameComp = this.LastName.CompareTo(st2.LastName);
+            if (lNameComp != 0)
+            {
+                return lNameComp;
+            }
+            int ssnComp = (int.Parse(this.SSN)).CompareTo(int.Parse(st2.SSN));
+            return ssnComp;
+        }
+    }
+}
+
---
+++ OOP/06.CommonTypeSystem/01-03.StudentClass-IClonable-IComparable/TestingStudentClass.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_03.StudentClass_IClonable_IComparable
+{
+    class TestingStudentClass
+    {
+        static void Main()
+        {
+            Student st1 = new Student("Grigor", "Antonov", "Pipkov", "102506", "Pernik", "088339988523",
+    2, Student.Specialties.SSS, Student.Univercities.UACEG, Student.Faculties.SF);
+            Console.WriteLine(st1.GetHashCode());
+            Console.WriteLine(st1.ToString());
+
+            Student st2 = new Student("Petya", "Radeva", "Molova", "130106", "Primorsko", "0883556688764",
+                3, Student.Specialties.Hidro, Student.Univercities.UACEG, Student.Faculties.HF);
+
+            Console.WriteLine(st1 == null);
+            Console.WriteLine(st1 != st2);
+            Console.WriteLine(st1 == st2);
+            Console.WriteLine(st1.Equals(st2));
+
+            Student st3 = (Student)st1.Clone();
+
+            st1.FirstName = "Kircho";
+            Console.WriteLine(st3.ToString());
+            Console.WriteLine(st1.ToString());
+            Console.WriteLine(st1.CompareTo(st3));
+            Console.WriteLine(st1.CompareTo(st1));
+        }
+    }
+}
